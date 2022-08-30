0eaed96c59c4a007c0878432953282b0a1f98fb3
Gideon Chrapko <gideonchrapko@hotmail.com>
2022-08-30 14:35:58 -0400

    adding return policy stuff

--- src/Components/ReturnPolicy.js
+++ src/Components/ReturnPolicy.js
@@ -3,19 +3,33 @@ import Nav from '../Components/Nav';
 import Cart from '../Components_shopify/Cart';
 import Footer from '../Components/Footer';
 
+import '../Styles/ReturnPolicy.css'
+
 import Background from '../Assets/SDpixelpng.png'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const ReturnPolicy = () => {
     return (
-        <div
+        <Container
+            fluid
             style={{ backgroundImage: `url(${Background})`, height: "100vh", width: "100%" }} 
             className='home-container' 
-        >
-            <h1>Return Policy</h1>
+        >   <Row style={{ paddingTop: "20vh" }}>
+                <Col lg={{ offset: 2, span: 8 }}>
+                    <h1 className='return-header'>Return Policy</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h4>
+
+                    </h4>
+                </Col>
+            </Row>
             <Nav /> 
             <Cart />
             <Footer />
-        </div>
+        </Container>
     )
 }
 
