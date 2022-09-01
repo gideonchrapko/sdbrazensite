7e19c4179d0ed14fca26d5ef02f9b316d46dc6da
Gideon Chrapko <gideonchrapko@hotmail.com>
2022-09-01 14:28:06 -0400

    adding nav bar opacity

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -15,7 +15,7 @@ const Nav = () => {
             <div style={{ position: "fixed" , top: "0", zIndex: "98", width: "100vw" }}>
                 {mobWidth ? 
                     <div onClick={() => setHover(!hover)} >
-                        <div style={{ opacity: `${hover ? "1" : "1" }` }} className='nav-div'>
+                        <div style={{ opacity: `${hover ? "1" : "1" }`, backgroundColor: hover ? "rgba(255,255,255,0.9)" : "" }} className='nav-div'>
                             <img src={Info} className={hover ? "info-hovered" : "info-nav"} />
                             <div style={{ marginTop: "20px" }}>
                                 <h1 className='nav-text' style={{ marginTop: "5px", opacity: `${hover ? "1" : "0"}` }}>CONTACT</h1>
@@ -25,7 +25,7 @@ const Nav = () => {
                     </div>
                     :
                     <div onPointerOver={() => setHover(true)} onPointerOut={() => setHover(false)}>
-                        <div style={{ opacity: `${hover ? "1" : "1" }` }} className='nav-div'>
+                        <div style={{ opacity: `${hover ? "1" : "1" }`, backgroundColor: hover ? "rgba(255,255,255,0.9)" : "" }} className='nav-div'>
                             <img src={Info} className={hover ? "info-hovered" : "info-nav"} />
                             <div style={{ marginTop: "20px" }}>
                                 <h1 className='nav-text' style={{ marginTop: "5px", opacity: `${hover ? "1" : "0"}` }}>CONTACT</h1>
