061a2b71e298abccbbead845e35b7a909356d37e
gideonchrapko <gideonchrapko@hotmail.ca>
2024-09-19 14:43:29 -0400

    adding

--- src/Components_shopify/Product.js
+++ src/Components_shopify/Product.js
@@ -1,167 +1,320 @@
-import { useEffect, useState, useRef } from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import { useShopify } from "../hooks"
-import { useDrag } from "@use-gesture/react"
-import clamp from "lodash.clamp"
-import useMeasure from "react-use-measure"
-import { useSpring, animated } from "@react-spring/web"
+import React, { useEffect, useState } from 'react';
+import { useShopify } from '../hooks';
+import { Row, Col, Container } from 'react-bootstrap';
 
-import SizeVariant from "./SizeVariants"
+import { useParams } from 'react-router-dom';
+// import SizeVariant from './SizeVariants';
 
-function Product(props) {
-  const { product, price, description } = props
-  const [index, setIndex] = useState(0)
-  const [end, setEnd] = useState(false)
-  const [ref, { width }] = useMeasure()
-  const val = product && product.images[0]
-  const countries = product && product.images
-  const arr3 = countries.concat(val)
-  const arr3Length = arr3.length - 1
-  const i = useRef(0)
-  const length = Array.from(Array(product && product.images.length).keys())
+export default function Product() {
+  const { product, fetchProduct, openCart, checkoutState, addVariant } =
+    useShopify();
 
-  const bind = useDrag(
-    ({
-      movement: [mx],
-      direction: [xDir],
-      distance,
-      dragging,
-      tap,
-      cancel,
-    }) => {
-      if (dragging && distance[0] > width / 2) {
-        i.current = clamp(i.current + (xDir > 0 ? -1 : 1), 0, arr3Length)
-        setIndex(i.current)
-        setEnd(false)
-        cancel()
-      }
-      if (!dragging && !tap && i.current === arr3Length) {
-        setEnd(true)
-      }
-      if (tap && i.current !== arr3Length) {
-        i.current = i.current + 1
-        setIndex(i.current)
-        setEnd(false)
-      }
-      if (tap && i.current === arr3Length) {
-        setEnd(true)
-      }
-      api.start({
-        to: {
-          x: dragging
-            ? -(width * i.current) + (i.current === arr3Length ? 0 : mx)
-            : -(width * i.current),
-        },
-        config: { duration: 250 },
-      })
+  const { productHandle } = useParams();
+  // const defaultSize = product.variants && product.variants[0].id.toString();
+  const [size, setSize] = useState('');
+  const [click, setClicked] = useState(false);
+  const [available, setAvailable] = useState(true);
+  const [sizeSelected, setSizeSelected] = useState(false);
+  // const [sizeTitle, setSizeTitle] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const description = product.description && product.description.split('.');
+
+  function changeSize(sizeId, quantity) {
+    openCart();
+    if (sizeId === '') {
+      sizeId = product.variants[0].id;
+      const lineItemsToAdd = [
+        { variantId: sizeId, quantity: parseInt(quantity, 10) },
+      ];
+      const checkoutId = checkoutState.id;
+      addVariant(checkoutId, lineItemsToAdd);
+    } else {
+      const lineItemsToAdd = [
+        { variantId: sizeId, quantity: parseInt(quantity, 10) },
+      ];
+      const checkoutId = checkoutState.id;
+      addVariant(checkoutId, lineItemsToAdd);
     }
-  )
+  }
 
   useEffect(() => {
-    if (end) {
-      setTimeout(() => {
-        api.start({
-          to: { x: 0 },
-          config: { duration: 0 },
-        })
-      }, [250])
-      i.current = 0
-      setIndex(0)
-    }
-  }, [end])
-
-  const [{ x }, api] = useSpring(() => ({
-    from: { x: 0 },
-  }))
+    fetchProduct(productHandle);
+  }, [productHandle]);
 
   return (
-    <Col
-      lg={{
-        span: 4,
-        offset: 4,
-      }}
-      xs={{ span: 12, offset: 0 }}
-      key={product.id + i}
-      style={{ marginBottom: "5vh", marginTop: "5vh" }}
-    >
-      <div className="images-product-bind">
-        {arr3?.map((images, i) => {
-          return (
-            <animated.img
-              {...bind()}
-              ref={ref}
-              key={i}
-              src={images.src}
-              className="img-drag"
-              style={{ x }}
-            />
-          )
-        })}
-      </div>
-      <div className="container-dot-two">
-        <div className="container-dot">
-          {length &&
-            length.map((dot, i) => {
+    <Container fluid>
+      <Row style={{ fontFamily: 'lucon' }}>
+        <Col
+          sm={{ span: 12 }}
+          md={{ span: 7 }}
+          style={{
+            height: '100vh',
+            padding: '100px',
+            overflow: 'scroll',
+          }}
+        >
+          {product.images &&
+            product.images.map((image, i) => {
               return (
-                <span
-                  style={{ background: index === i ? "black" : "#999" }}
-                  key={dot}
-                  className="dot"
-                ></span>
-              )
+                <img
+                  key={image.id + i}
+                  src={image.src}
+                  alt={`${product.title} product shot`}
+                />
+              );
             })}
-        </div>
-      </div>
-      <div
-        style={{
-          width: "100%",
-          textAlign: "center",
-        }}
-      >
-        <h1 className="product-new-title">{product.title}</h1>
-        <h3 className="Prod-price-font">${price}0</h3>
-        <SizeVariant product={product} description={description} i={i} />
-      </div>
-    </Col>
-  )
+        </Col>
+        <Col style={{ height: '100vh' }}>
+          <div
+            style={{
+              height: '100%', // Fixed height for the parent div
+              display: 'flex',
+              alignItems: 'center', // Vertically centers the child div
+              justifyContent: 'center', // Horizontally centers the child div (optional)
+            }}
+          >
+            <div
+              style={{
+                width: '100%',
+                paddingLeft: '40px',
+                paddingRight: '40px',
+              }}
+            >
+              <div>
+                <h1 style={{ textDecoration: 'underline', fontSize: '20px' }}>
+                  {product.title}
+                </h1>
+                <h3 style={{ fontSize: '20px', paddingTop: '10px' }}>
+                  ${product.variants && product.variants[0].price.amount}
+                </h3>
+                <h3 style={{ fontSize: '20px', paddingTop: '10px' }}>
+                  Shipping Calculated at checkout
+                </h3>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center', // Vertically center the items
+                    fontSize: '20px',
+                  }}
+                >
+                  Size:
+                  {product.variants &&
+                    product.variants.map((variant, item) => {
+                      return (
+                        <div
+                          key={variant.title + item}
+                          onClick={(e) => {
+                            setSize(variant.id.toString());
+                            setClicked(item);
+                            setAvailable(variant.available);
+                            setSizeSelected(true);
+                          }}
+                          style={{
+                            paddingLeft: '40px',
+                            cursor: 'pointer',
+                            color: variant.available
+                              ? `${click === item ? '#FF09B1' : 'black'}`
+                              : 'grey',
+                          }}
+                        >
+                          {variant.title}
+                        </div>
+                      );
+                    })}
+                </div>
+
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center', // Vertically center the items
+                    fontSize: '20px',
+                  }}
+                >
+                  Quantity:
+                  <div
+                    className="prodQuantity-container"
+                    style={{ paddingLeft: '20px' }}
+                  >
+                    {quantity > 1 ? (
+                      <button
+                        className="prodQuantity-update"
+                        onClick={() => setQuantity(quantity - 1)}
+                      >
+                        -
+                      </button>
+                    ) : (
+                      <button className="prodQuantity-update">-</button>
+                    )}
+                    <span className="prodQuantity" style={{ color: 'black' }}>
+                      {quantity}
+                    </span>
+                    <button
+                      className="prodQuantity-update"
+                      onClick={() => setQuantity(quantity + 1)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+
+                <div
+                  style={{
+                    fontSize: '20px',
+                    listStyleType: 'none',
+                    paddingTop: '10px',
+                  }}
+                >
+                  {description &&
+                    description.map((each, i) => {
+                      return <li key={`line-description +${i}`}>{each}</li>;
+                    })}
+                </div>
+                {sizeSelected && available ? (
+                  <button
+                    style={{
+                      background: 'black',
+                      color: 'white',
+                      width: '200px',
+                      marginTop: '10px',
+                      fontSize: '20px',
+                      border: 'none',
+                    }}
+                    onClick={() => changeSize(size, quantity)}
+                  >
+                    Add to cart
+                  </button>
+                ) : (
+                  <button
+                    style={{
+                      background: 'black',
+                      color: 'white',
+                      width: '200px',
+                      marginTop: '10px',
+                      fontSize: '20px',
+                      border: 'none',
+                    }}
+                  >
+                    {sizeSelected ? 'Out of Stock' : 'Select a Size'}
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row></Row>
+    </Container>
+  );
 }
 
-export default function Products() {
-  const { products, fetchProduct } = useShopify()
-  const id = "id"
-
-  useEffect(() => {
-    fetchProduct(id)
-  }, [id])
-
-  return (
-    <Container
-      fluid
-      style={{
-        zIndex: "10",
-        overflow: "hidden",
-        height: "100%",
-      }}
-    >
-      <Row>
-        {products &&
-          products.map((product, i) => {
-            const image = product.images[0].src
-            const price = product.variants[0].price.amount
-            const description =
-              product.description && product.description.split(".")
-            return (
-              <Product
-                i={i}
-                key={i}
-                image={image}
-                price={price}
-                description={description}
-                product={product}
+{
+  /* <Row className="Product-wrapper2">
+  <Col
+    className="Images"
+    style={{ marginLeft: '30px', background: 'red' }}
+    sm={{ span: 12 }}
+    lg={{ span: 6 }}
+    md={{ span: 6 }}
+  >
+    {product.images &&
+      product.images.map((image, i) => {
+        return (
+          <img
+            className="Product_Image"
+            key={image.id + i}
+            src={image.src}
+            alt={`${product.title} product shot`}
+          />
+        );
+      })}
+  </Col>
+  <Col className="Product__info">
+    <h2 className="Product__title2">{product.title}</h2>
+    <h3 className="Productview__price">
+      ${product.variants && product.variants[0].price}
+    </h3>
+    <label htmlFor={'prodOptions'} style={{ marginTop: '2%' }}>
+      Size
+    </label>
+    <br />
+    <div style={{ width: '90%', position: 'relative' }}>
+            <div
+              className="style__dropdown"
+              id="prodOptions"
+              onClick={(e) => {
+                setdropDownMenu(!dropDownMenu);
+                setRotate(!rotate);
+              }}
+            >
+              {sizeTitle ? sizeTitle : 'Pick a Size'}
+              <animated.img
+                src={DropDownArrow}
+                alt="drop down arrow"
+                style={rotationAnimation}
+                className="dropDownArrow"
               />
-            )
-          })}
-      </Row>
-    </Container>
-  )
+            </div>
+            <animated.div
+              className="style__dropdownDiv"
+              style={dropDownMenuAnimation}
+            >
+              {product.variants &&
+                product.variants.map((item, i) => {
+                  return (
+                    <li
+                      onClick={(e) => {
+                        clickFunction(item, i);
+                      }}
+                      className={
+                        item.available ? 'size__option' : 'size__option2'
+                      }
+                      key={item.title + i}
+                    >{`${item.title}`}</li>
+                  );
+                })}
+            </animated.div>
+          </div>
+    <div style={{ marginTop: '2%' }}>
+      <label>Quantity</label>
+      <br />
+      <div className="prodQuantity-container">
+              {quantity > 1 ? (
+                <button
+                  className="prodQuantity-update"
+                  onClick={() => setQuantity(quantity - 1)}
+                >
+                  -
+                </button>
+              ) : (
+                <button className="prodQuantity-update">-</button>
+              )}
+              <span className="prodQuantity" style={{ color: 'white' }}>
+                {quantity}
+              </span>
+              <button
+                className="prodQuantity-update"
+                onClick={() => setQuantity(quantity + 1)}
+              >
+                +
+              </button>
+            </div>
+    </div>
+    {product.availableForSale ? (
+      <button
+        className="prodBuyButton"
+        onClick={(e) => changeSize(size, quantity)}
+      >
+        Add to Cart
+      </button>
+    ) : (
+      <button className="prodBuyButtonSold">Sold Out</button>
+    )}
+    <div className="Product__description">
+      {description &&
+              description.map((each, i) => {
+                return <li key={`line-description +${i}`}>{each}</li>;
+              })}
+    </div>
+  </Col>
+</Row>; */
 }
--- src/redux/ducks/shopify/index.js
+++ src/redux/ducks/shopify/index.js
@@ -1,5 +1,5 @@
-import { useSelector, useDispatch } from "react-redux"
-import Client from "shopify-buy"
+import { useSelector, useDispatch } from 'react-redux';
+import Client from 'shopify-buy';
 
 // const client = Client.buildClient({
 //   storefrontAccessToken: "dd4d4dc146542ba7763305d71d1b3d38",
@@ -7,21 +7,21 @@ import Client from "shopify-buy"
 // })
 
 const client = Client.buildClient({
-  storefrontAccessToken: "7139a1b0c3a5fec6dd28f380632ffbd7",
-  domain: "dabrazn1.myshopify.com",
-})
+  storefrontAccessToken: '7139a1b0c3a5fec6dd28f380632ffbd7',
+  domain: 'dabrazn1.myshopify.com',
+});
 
-const PRODUCTS_FOUND = "shopify/PRODUCTS_FOUND"
-const PRODUCT_FOUND = "shopify/PRODUCT_FOUND"
-const COLLECTION_FOUND = "shopify/COLLECTION_FOUND"
-const CHECKOUT_FOUND = "shopify/CHECKOUT_FOUND"
-const SHOP_FOUND = "shopify/SHOP_FOUND"
-const ADD_VARIANT_TO_CART = "shopify/ADD_VARIANT_TO_CART"
-const UPDATE_QUANTITY_IN_CART = "shopify/UPDATE_QUANTITY_IN_CART"
-const REMOVE_LINE_ITEM_IN_CART = "shopify/REMOVE_LINE_ITEM_IN_CART"
-const OPEN_CART = "shopify/OPEN_CART"
-const CLOSE_CART = "shopify/CLOSE_CART"
-const CART_COUNT = "shopify/CART_COUNT"
+const PRODUCTS_FOUND = 'shopify/PRODUCTS_FOUND';
+const PRODUCT_FOUND = 'shopify/PRODUCT_FOUND';
+const COLLECTION_FOUND = 'shopify/COLLECTION_FOUND';
+const CHECKOUT_FOUND = 'shopify/CHECKOUT_FOUND';
+const SHOP_FOUND = 'shopify/SHOP_FOUND';
+const ADD_VARIANT_TO_CART = 'shopify/ADD_VARIANT_TO_CART';
+const UPDATE_QUANTITY_IN_CART = 'shopify/UPDATE_QUANTITY_IN_CART';
+const REMOVE_LINE_ITEM_IN_CART = 'shopify/REMOVE_LINE_ITEM_IN_CART';
+const OPEN_CART = 'shopify/OPEN_CART';
+const CLOSE_CART = 'shopify/CLOSE_CART';
+const CART_COUNT = 'shopify/CART_COUNT';
 
 const initialState = {
   isCartOpen: false,
@@ -31,36 +31,36 @@ const initialState = {
   featured: [],
   product: {},
   shop: {},
-}
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case PRODUCTS_FOUND:
-      return { ...state, products: action.payload }
+      return { ...state, products: action.payload };
     case PRODUCT_FOUND:
-      return { ...state, product: action.payload }
+      return { ...state, product: action.payload };
     case COLLECTION_FOUND:
-      return { ...state, featured: action.payload }
+      return { ...state, featured: action.payload };
     case CHECKOUT_FOUND:
-      return { ...state, checkout: action.payload }
+      return { ...state, checkout: action.payload };
     case SHOP_FOUND:
-      return { ...state, shop: action.payload }
+      return { ...state, shop: action.payload };
     case ADD_VARIANT_TO_CART:
-      return { ...state, checkout: action.payload }
+      return { ...state, checkout: action.payload };
     case UPDATE_QUANTITY_IN_CART:
-      return { ...state, checkout: action.payload }
+      return { ...state, checkout: action.payload };
     case REMOVE_LINE_ITEM_IN_CART:
-      return { ...state, checkout: action.payload }
+      return { ...state, checkout: action.payload };
     case OPEN_CART:
-      return { ...state, isCartOpen: true }
+      return { ...state, isCartOpen: true };
     case CLOSE_CART:
-      return { ...state, isCartOpen: false }
+      return { ...state, isCartOpen: false };
     case CART_COUNT:
-      return { ...state, cartCount: action.payload }
+      return { ...state, cartCount: action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
 
 // Gets all the products from Shopify
 function getProducts() {
@@ -69,22 +69,22 @@ function getProducts() {
       dispatch({
         type: PRODUCTS_FOUND,
         payload: resp,
-      })
-    })
-  }
+      });
+    });
+  };
 }
 
 // Gets individual item based on id
-function getProduct(id) {
+function getProduct(handle) {
   // console.log
   return async (dispatch) => {
-    const resp = await client.product.fetch(id)
+    const resp = await client.product.fetchByHandle(handle);
     dispatch({
       type: PRODUCT_FOUND,
       payload: resp,
-    })
-    return resp
-  }
+    });
+    return resp;
+  };
 }
 
 //
@@ -109,9 +109,9 @@ function checkout() {
       dispatch({
         type: CHECKOUT_FOUND,
         payload: resp,
-      })
-    })
-  }
+      });
+    });
+  };
 }
 
 // Gets Shopify store information
@@ -121,9 +121,9 @@ function shopInfo() {
       dispatch({
         type: SHOP_FOUND,
         payload: resp,
-      })
-    })
-  }
+      });
+    });
+  };
 }
 
 // Adds variants to cart/checkout
@@ -132,32 +132,32 @@ function addVariantToCart(checkoutId, lineItemsToAdd) {
     const response = await client.checkout.addLineItems(
       checkoutId,
       lineItemsToAdd
-    )
+    );
     dispatch({
       type: ADD_VARIANT_TO_CART,
       payload: response,
-    })
-    return response
-  }
+    });
+    return response;
+  };
 }
 
 // Updates quantity of line items in cart and in checkout state
 function updateQuantityInCart(lineItemId, quantity, checkoutId) {
   const lineItemsToUpdate = [
     { id: lineItemId, quantity: parseInt(quantity, 10) },
-  ]
+  ];
 
   return async (dispatch) => {
     const resp = await client.checkout.updateLineItems(
       checkoutId,
       lineItemsToUpdate
-    )
+    );
     dispatch({
       type: UPDATE_QUANTITY_IN_CART,
       payload: resp,
-    })
-    return resp
-  }
+    });
+    return resp;
+  };
 }
 
 // Removes line item from cart and checkout state
@@ -167,23 +167,23 @@ function removeLineItemInCart(checkoutId, lineItemId) {
       dispatch({
         type: REMOVE_LINE_ITEM_IN_CART,
         payload: resp,
-      })
-    })
-  }
+      });
+    });
+  };
 }
 
 // To close the cart
 function handleCartClose() {
   return {
     type: CLOSE_CART,
-  }
+  };
 }
 
 // To open the cart
 function handleCartOpen() {
   return {
     type: OPEN_CART,
-  }
+  };
 }
 
 // Set the count of items in the cart
@@ -191,35 +191,37 @@ function handleSetCount(count) {
   return {
     type: CART_COUNT,
     payload: count,
-  }
+  };
 }
 
 export function useShopify() {
-  const dispatch = useDispatch()
-  const cartStatus = useSelector((appState) => appState.shopifyState.isCartOpen)
-  const cartCount = useSelector((appState) => appState.shopifyState.cartCount)
-  const products = useSelector((appState) => appState.shopifyState.products)
-  const product = useSelector((appState) => appState.shopifyState.product)
-  const featured = useSelector((appState) => appState.shopifyState.featured)
+  const dispatch = useDispatch();
+  const cartStatus = useSelector(
+    (appState) => appState.shopifyState.isCartOpen
+  );
+  const cartCount = useSelector((appState) => appState.shopifyState.cartCount);
+  const products = useSelector((appState) => appState.shopifyState.products);
+  const product = useSelector((appState) => appState.shopifyState.product);
+  const featured = useSelector((appState) => appState.shopifyState.featured);
   const checkoutState = useSelector(
     (appState) => appState.shopifyState.checkout
-  )
-  const shopDetails = useSelector((appState) => appState.shopifyState.shop)
-  const fetchProducts = () => dispatch(getProducts())
-  const fetchProduct = (id) => dispatch(getProduct(id))
+  );
+  const shopDetails = useSelector((appState) => appState.shopifyState.shop);
+  const fetchProducts = () => dispatch(getProducts());
+  const fetchProduct = (handle) => dispatch(getProduct(handle));
   // const fetchCollection = () => dispatch(getCollection())
-  const createCheckout = () => dispatch(checkout())
-  const createShop = () => dispatch(shopInfo())
-  const closeCart = () => dispatch(handleCartClose())
-  const openCart = () => dispatch(handleCartOpen())
-  const setCount = (count) => dispatch(handleSetCount(count))
+  const createCheckout = () => dispatch(checkout());
+  const createShop = () => dispatch(shopInfo());
+  const closeCart = () => dispatch(handleCartClose());
+  const openCart = () => dispatch(handleCartOpen());
+  const setCount = (count) => dispatch(handleSetCount(count));
 
   const addVariant = (checkoutId, lineItemsToAdd) =>
-    dispatch(addVariantToCart(checkoutId, lineItemsToAdd))
+    dispatch(addVariantToCart(checkoutId, lineItemsToAdd));
   const updateQuantity = (lineItemId, quantity, checkoutID) =>
-    dispatch(updateQuantityInCart(lineItemId, quantity, checkoutID))
+    dispatch(updateQuantityInCart(lineItemId, quantity, checkoutID));
   const removeLineItem = (checkoutId, lineItemId) =>
-    dispatch(removeLineItemInCart(checkoutId, lineItemId))
+    dispatch(removeLineItemInCart(checkoutId, lineItemId));
 
   return {
     products,
@@ -240,5 +242,5 @@ export function useShopify() {
     updateQuantity,
     removeLineItem,
     setCount,
-  }
+  };
 }
